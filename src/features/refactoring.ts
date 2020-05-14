c42ac9f140a2b70f4f62b29f808e57dc8cb6480c
Kirill Valikov <Kirill.Valikov@infowatch.com>
2020-05-14 21:53:56 +0300

    add ts->tree for renaming;

--- src/features/refactoring.ts
+++ src/features/refactoring.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import * as Proto from 'typescript/lib/protocol';
+import * as path from "path";
 
 import { TsServer } from '../tsserver';
 import { TREE_PATTERN } from '../utils/model';
 import { getSourceMap, getNameExceptExtension, getPathNameExceptExtension } from '../utils/functions';
+import { BasicSourceMapConsumer } from 'source-map';
 
-const SPECIAL_SYMBOLS = /(<=>|<=|=>|\*|\\|\/|@)/;
+const METHOD_SYMBOLS = /(<=>|<=|=>)/;
 
 export function refactoring(server: TsServer, context: vscode.ExtensionContext) {
     context.subscriptions.push(treeToTsRename(server))
@@ -27,11 +29,25 @@ function treeToTsRename(server: TsServer){
                 
             }else {
                 const edit = generateWorkspaceEdit(response.body.locs, newName);
-                edit.replace(document.uri, document.getWordRangeAtPosition(position)!, newName);
+                addTreeFilesToEdit(response.body.locs, edit, newName, sourceMap);
                 return edit;
             }
         }
     })
+}
+
+function addTreeFilesToEdit(locations: readonly Proto.SpanGroup[], edit: vscode.WorkspaceEdit, newName: string, sourceMap: BasicSourceMapConsumer){
+    for (const spanGroup of locations) {
+        if (!spanGroup.file.endsWith("*.tree.ts")) return;
+        const pathName = spanGroup.file.split(path.sep+"view.tree"+ path.sep);
+        const resource = vscode.Uri.parse(pathName[0]+path.sep+pathName[1].substring(0, -3));
+        for (const textSpan of spanGroup.locs) {
+            const start = sourceMap.originalPositionFor({ column:textSpan.start.offset, line: textSpan.start.line})
+            const end = sourceMap.originalPositionFor({ column:textSpan.end.offset, line: textSpan.end.line})
+            const range = new vscode.Range(new vscode.Position(start.line!, start.column!), new vscode.Position(end.line!, end.column!))
+            edit.replace(resource, range, newName);
+        }
+    }
 }
 
 function generateWorkspaceEdit(
